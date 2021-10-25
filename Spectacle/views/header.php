87e51d7ee0fb38238c2c3e878d288b73fc118fc1
aymerick <aymericksh@hotmail.com>
2021-10-25 12:17:04 +0200

    header modifier rien de fou

--- Spectacle/views/header.php
+++ Spectacle/views/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">
             <img src="assets/img/logo.png" alt="logo Blog">
@@ -6,22 +6,46 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarText">
+        <div class="collapse navbar-collapse text-center" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link" href="index.php?page=home">Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=posts">Articles</a>
+                    <a class="nav-link" href="index.php?page=posts">Type1</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=contact">Contact</a>
+                    <a class="nav-link" href="index.php?page=contact">Type2</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=donate">Faire un don</a>
+                    <a class="nav-link" href="index.php?page=donate">Type3</a>
                 </li>
-            </ul>
-            <span class="navbar-text">Blog en PHP procédural</span>
+            </ul>  
         </div>
     </div>
+</nav> -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid coll">
+    <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="logo Blog"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php?page=home">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?page=posts">Type1</a>
+        </li>
+        <li class="nav-item">
+                    <a class="nav-link" href="index.php?page=contact">Type2</a>
+        </li>
+        <li class="nav-item">
+                    <a class="nav-link" href="index.php?page=donate">Type3</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
 </nav>
